c6bb8ee6b5d6d00f3f9b0500f58d3886c509eade
ng-peter <31899876+ng-peter@users.noreply.github.com>
2018-03-18 13:20:52 -0400

    Create demo.js

---
+++ Class06/demo.js
@@ -0,0 +1,41 @@
+'use strict';
+
+
+
+
+
+
+//string manipulation demo
+
+var strExample = "This is a string and a string is long.";
+
+console.log(strExample);
+
+console.log(strExample.toUpperCase());
+console.log(strExample.toLowerCase());
+
+console.log(strExample.substr(3));
+console.log(strExample.substr(3, 8));
+
+console.log(strExample.substring(23));
+console.log(strExample.substring(23, 31));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// EOF - END OF FILE
