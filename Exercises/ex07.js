5d351ba03d6cadf876c1e60d8ced265d48088e45
ng-peter <31899876+ng-peter@users.noreply.github.com>
2018-03-20 21:52:53 -0400

    Create ex07.js

---
+++ Exercises/ex07.js
@@ -0,0 +1,93 @@
+"use strict";
+
+/*
+PART 1:
+Write a fucntion that takes a book object as ARGUMENT
+and RETURN an output with the price of the book after taxes
+- assume taxes is 15%
+- do not worry about rounding decimals
+
+After the function is created, use it on the Array books and 
+output the book name and the price after taxes
+*/
+
+function afterTax(argumentHere){
+  //to be completed
+}
+
+var book1 = { name:"Harry Potter and the Sorcerer's Stone", price: 12.73 };
+var book2 = { name:"The Lord of the Rings", price: 29.66 };
+var book3 = { name:"In Search of Lost Time", price: 21.76 };
+var book4 = { name:"A Game of Thrones", price: 11.69 };
+var book5 = { name:"Trump: The Art of the Deal", price: 19.79 };
+
+var books = [book1,book2,book3,book4,book5];
+
+/*
+PART 2:
+Create a fucntion that takes an array as ARGUMENT
+and RETURN the TOTAL price of all books inside the array after taxes
+(you can reuse afterTax within this function to facilitate the task)
+*/
+
+function priceTotalAfterTax(argumentHere){
+  //to be completed
+}
+
+/*
+PART3:
+Revise and generalize the afterTax and priceTotalAfterTax functions
+to take TWO ARGUMENTS, one: same as before, the other: a choice of tax values
+(tax values predefined for you in the list below)
+- if you wish, you can rename these functions with a slightly different name
+*/
+
+var taxAB = 5,
+  taxBC = 12,
+  taxMB = 13,
+  taxNB = 15,
+  taxNL = 15,
+  taxNT = 5,
+  taxNS = 15,
+  taxNU = 5,
+  taxON = 13,
+  taxPE = 15,
+  taxQC = 14.975,
+  taxSK = 11,
+  taxYT = 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// EOF - END OF FILE
