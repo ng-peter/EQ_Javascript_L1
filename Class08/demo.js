b91bbd1c8be1d3adc910e3a605923f609d687ab7
ng-peter <31899876+ng-peter@users.noreply.github.com>
2018-03-28 08:11:16 -0400

    Create demo.js

---
+++ Class08/demo.js
@@ -0,0 +1,56 @@
+'use strict';
+
+
+
+
+
+var demoObj = {
+  title:"A movie",
+  duration:120,
+  getTime:function(){
+    return this.duration*2;
+  }
+}
+
+
+
+const plusOne = (num) => num + 1;
+
+const plusTwo = (num) => plusOne(num+1);
+
+const printOut = () => { console.log("something") };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// EOF - END OF FILE
