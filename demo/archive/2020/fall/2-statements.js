ea8731ca1a5247f9a4e3d8b50c8fafdf1fa3bb03
Peter Ng <ng.peter@gmail.com>
2020-10-08 22:24:25 -0400

    add statements in class demo

---
+++ demo/archive/2020/fall/2-statements.js
@@ -0,0 +1,37 @@
+var name = "peter";
+console.log(123);
+
+var num1 = 18;
+num1 %= num1 + 1;
+
+num1 + 1; // 19
+num1 % 19; // 18 % 19 = 18
+num1 = 18;
+
+num1 %= num1 % 5;
+
+num1 % 5; // 3
+num1 % num1; // 3 % 3 = 0
+num1 = 0;
+
+var i = 1; // i = 1
+var j = i++;
+
+var j = i; // j = i = 1
+i++; // i += 1 = 2
+
+var i = 1; // i = 1
+var j;
+j += ++i;
+
+var i = 1; // i = 1
+var j = ++i; // i = 2, j = 2
+j += ++j; // j = jl(2) + ++jr(3) = 5
+
+var i = 1; // i = 1
+var j = i++; // i = 2, j = 1
+j += j++; // 2
+
+var i = 1; // i = 1
+var j = ++i; // i = 2, j = 2
+j += j++; // 4
